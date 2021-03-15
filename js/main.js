d8ff9dc45dbb05fa41db8898fb8a09acc4d8d938
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-03-15 16:13:22 -0300

    gamer

--- js/main.js
+++ js/main.js
@@ -241,6 +241,7 @@ $(document).ready(function(){
       })
 
 
+      
 
     //----- Active No ui slider --------//
 
@@ -558,8 +559,30 @@ $(document).ready(function(){
           ]
         });
       }
-
-
   
 
  });
+
+
+ // Porfolio isotope and filter
+ $(window).on('load', function() {
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on('click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({
+      filter: $(this).data('filter')
+    });
+    aos_init();
+  });
+
+  // Initiate venobox (lightbox feature used in portofilo)
+  $(document).ready(function() {
+    $('.venobox').venobox();
+  });
+});
