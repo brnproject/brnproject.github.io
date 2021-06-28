f5d4f0f534f4a6014eb677328774da2f4c159f50
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-06-28 14:22:45 -0300

    site Anjo da Guarda

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,14 @@
 <?php
+   //Variáveis que recebem os dados digitados no formulário pelo id atribuído nos input
+   $from = $_REQUEST['email'];
+   $name = $_REQUEST['name'];
+   $csubject = $_REQUEST['subject'];
+   $cmessage = $_REQUEST['message'];
 
-    $to = "atendimento@infob2.com.br";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $csubject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
-
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-    $subject = "Você tem uma mensagem de contato do site B2 Informática.";
-
-    $logo = 'img/logo.png';
-    $link = '#';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-
+mail (
+    "mtbernioficial@gmail.com", //Endereço que vai receber a mensagem
+    "Nome: $name
+     Email: $from
+     Assunto: $csubject
+     Mensagem: $cmessage", "FROM:$name<$from>");
 ?>
